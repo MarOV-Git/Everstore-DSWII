686852d98b299e87c9bf6976c96cc01d495ee717
MarOV-Git <martin.m4ple@gmail.com>
2021-11-20 02:06:34 -0500

    Minificado

--- js/config/configFirebase.js
+++ js/config/configFirebase.js
@@ -1,9 +1,1 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDt4wEGrvTxLTnS6rCm7Wn9GIX9pBOAq2M",
-    authDomain: "everstore-fba41.firebaseapp.com",
-    projectId: "everstore-fba41",
-    storageBucket: "everstore-fba41.appspot.com",
-    messagingSenderId: "524413926954",
-    appId: "1:524413926954:web:f1740f86c244a21e310d53",
-    measurementId: "G-EYS84FH6ZT"
-};
+const firebaseConfig={apiKey:"AIzaSyDt4wEGrvTxLTnS6rCm7Wn9GIX9pBOAq2M",authDomain:"everstore-fba41.firebaseapp.com",projectId:"everstore-fba41",storageBucket:"everstore-fba41.appspot.com",messagingSenderId:"524413926954",appId:"1:524413926954:web:f1740f86c244a21e310d53",measurementId:"G-EYS84FH6ZT"};
